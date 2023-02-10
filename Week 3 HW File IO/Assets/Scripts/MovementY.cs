0fa3dce9ab17c7fecbfe19eb2241f5184f1ca095
DESKTOP-J8O6J48\Yasmine <yasmine.batniji@gmail.com>
2023-02-10 14:19:34 -0500

    added game manager and movement script

---
+++ Week 3 HW File IO/Assets/Scripts/MovementY.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementY : MonoBehaviour
+{
+    //how fast obj moves
+    public float speed = 2;
+    
+    //initial position
+    private Vector3 initialPos;
+
+    private Transform cachedTransform;
+
+    public int resetPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cachedTransform = transform;
+        initialPos = cachedTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+
+        newPos.y += speed * Time.deltaTime;
+
+        transform.position = newPos;
+
+        if (cachedTransform.position.x >= resetPosition)
+        {
+            cachedTransform.position = initialPos;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
